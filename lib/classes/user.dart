4a00ece590ba285d880ad7bc6561a20400788e95
Ziad <ziad.assem2001@gmail.com>
2022-11-06 23:01:26 +0200

    auth

--- lib/classes/user.dart
+++ lib/classes/user.dart
@@ -5,12 +5,10 @@ class User {
   const User({
     required this.uid,
     required this.email,
-    required this.photoUrl,
     required this.displayName,
   });
 
   final String uid;
   final String email;
-  final String photoUrl;
   final String displayName;
 }
--- lib/utils/authentication.dart
+++ lib/utils/authentication.dart
@@ -8,16 +8,16 @@ class FirebaseAuthService {
   final GoogleSignIn _googleSignIn;
 
   FirebaseAuthService({required FirebaseAuth firebaseAuth, required GoogleSignIn googleSignin})
-      : _firebaseAuth = firebaseAuth ?? FirebaseAuth.instance,
-        _googleSignIn = googleSignin ?? GoogleSignIn();
+      : _firebaseAuth = firebaseAuth, //?? FirebaseAuth.instance,
+        _googleSignIn = googleSignin ;//?? GoogleSignIn();
 
-  u.User? _userFromFirebase(User user) {
+  User _userFromFirebase(User user) {
    
-    return u.User(
+    return User(
       uid: user.uid,
       email: user.email!,
       displayName: user.displayName!,
-      photoUrl: user.photoUrl
+      
     );
   }
 
@@ -25,20 +25,20 @@ class FirebaseAuthService {
     return _firebaseAuth.authStateChanges().map(_userFromFirebase);
   }
 
-  Future<User> signInAnonymously() async {
-    final authResult = await _firebaseAuth.signInAnonymously();
-    return _userFromFirebase(authResult.user);
-  }
+  // Future<User> signInAnonymously() async {
+  //   final authResult = await _firebaseAuth.signInAnonymously();
+  //   return _userFromFirebase(authResult.user);
+  // }
 
   Future<User> signInWithGoogle() async {
     final googleUser = await _googleSignIn.signIn();
-    final googleAuth = await googleUser.authentication;
-    final credential = GoogleAuthProvider.getCredential(
-      accessToken: googleAuth.accessToken,
-      idToken: googleAuth.idToken,
+    final googleAuth = await googleUser?.authentication;
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
     );
     final authResult = await _firebaseAuth.signInWithCredential(credential);
-    return _userFromFirebase(authResult.user);
+    return _userFromFirebase(authResult.user!);
   }
 
   Future<void> signOut() async {
@@ -46,7 +46,7 @@ class FirebaseAuthService {
   }
 
   Future<User> currentUser() async {
-    final user = await _firebaseAuth.currentUser();
+    final user = await _firebaseAuth.currentUser!;
     return _userFromFirebase(user);
   }
 }
